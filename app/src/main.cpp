5c7c61cd71e5a68d1782cecd03f4fe12c1d4e81e
DJviolin <kerozin.joe@gmail.com>
2016-08-01 20:06:26 +0200

    refact

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,8 @@
-#include <amalgamate/crow_all.h>
-
 //#define NOGDI  // Windows
+//#define WIN32_LEAN_AND_MEAN
+
+//#include <amalgamate/crow_all.h>
+#include <crow/crow.h>
 
 int main() {
   crow::SimpleApp app;
